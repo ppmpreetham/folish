a8d442f5ce1ca8a77cca4d0efcfd1c84a266250a
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-01-14 00:28:39 +0530

    add: more settings for brush

--- src/utils/brushConfig.ts
+++ src/utils/brushConfig.ts
@@ -1,6 +1,14 @@
 export const DEFAULT_BRUSH = {
-  thinning: 0.5,
   smoothing: 0.5,
+  thinning: 0.5,
   streamline: 0.5,
-  simulatePressure: false,
+  easing: (t: any) => t,
+  start: {
+    taper: 0,
+    cap: true,
+  },
+  end: {
+    taper: 0,
+    cap: true,
+  },
 }
